9ed1cfa74e8b68bef01c007c8ef38041cf1d4e06
smurfie <joanfluvia@gmail.com>
2020-09-03 10:16:38 +0200

    fix: analytics only in production

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const PORT = process.env.PORT || 3000;
+const ENV = process.env.NODE_ENV || 'localhost'; // 'production' in production
 
 let express = require('express'),
-  app = express();
+  app = express(),
+  isProduction = ENV === 'production';
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -14,30 +16,29 @@ app.use(express.urlencoded({ extended: true, limit:'1Mb', parameterLimit:10000 }
 app.use(express.json());
 
 app.get('/', function(req, res) {
-  res.render('home');
+  res.render('home', {isProduction: isProduction});
 });
 
 app.get('/home', function(req, res) {
-  res.render('home');
+  res.render('home', {isProduction: isProduction});
 });
 
 app.get('/game/:game?', function(req, res) {
-  res.render('game', {game: req.params.game || ''});
+  res.render('game', {game: req.params.game || '', isProduction: isProduction});
 });
 
 app.get('/editor', function(req, res) {
-  res.render('editor', {game: ''});
+  res.render('editor', {game: '', isProduction: isProduction});
 });
 
 app.get('/editor/:game', function(req, res) {
-  res.render('editor', {game: req.params.game});
+  res.render('editor', {game: req.params.game, isProduction: isProduction});
 });
 
 app.get('/canvas/:game?', function(req, res) {
-  res.render('canvas', {game: req.params.game || ''});
+  res.render('canvas', {game: req.params.game || '', isProduction: isProduction});
 });
 
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
-  console.log(`Env variables: ${JSON.stringify(process.env)}`);
 })
